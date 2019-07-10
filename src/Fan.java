e98fb71dcb70bb934b2f372632ce681372aca0ab
unknown <nguyenduc97hc>
2019-07-10 09:05:18 +0700

    first commit

---
+++ src/Fan.java
@@ -0,0 +1,86 @@
+public class Fan {
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+
+    private int speed ;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        this.speed = SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSLOW() {
+        return SLOW;
+    }
+
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public int getFAST() {
+        return FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    @Override
+    public String toString(){
+        String onOff = "Off";
+        if (on) {
+            onOff = "On";
+            return "fan is " + onOff + ":{" +
+                    "speed=" + speed +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        } else {
+            return "fan is " + onOff + ":{" +
+                    " radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Fan fan1 = new Fan(3, true, 10, "yellow");
+        Fan fan2 = new Fan(2, false, 5, "blue");
+
+        fan1.toString();
+        System.out.println( fan1.toString());
+        fan2.toString();
+        System.out.println( fan2.toString());
+
+    }
+}
